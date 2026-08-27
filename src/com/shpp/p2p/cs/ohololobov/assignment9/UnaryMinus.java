0c7c5983e434ca8b619f62290e9e99d5a6b8b696
Oleksandr Hololobov <alsan3004@gmail.com>
2026-08-28 00:34:58 +0200

    UnaryMinus. Add class. Add comments

---
+++ src/com/shpp/p2p/cs/ohololobov/assignment9/UnaryMinus.java
@@ -0,0 +1,26 @@
+package com.shpp.p2p.cs.ohololobov.assignment9;
+
+/**
+ * This class contains string and char values of unary minus
+ * although numeric string value of unary string multiplicator
+ * (the number that can be used to represent unary minus as
+ * multiplication)
+ */
+public class UnaryMinus {
+    /**
+     * string value of unary minus
+     */
+    public static final String VALUE = "-";
+
+    /**
+     * char value of unary minus
+     */
+    public static final char CHAR_VALUE = '-';
+
+    /**
+     * numeric string value of unary string multiplicator
+     * (the number that can be used to represent unary minus as
+     *  multiplication)
+     */
+    public static final String UNARY_MINUS_MULTIPLICATOR = "-1";
+}
