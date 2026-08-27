b21a02bf204b78f5c42ed6fb26a1c3dd41c9f99f
Oleksandr Hololobov <alsan3004@gmail.com>
2026-08-28 00:39:05 +0200

    VariableEqualityRecord. Add comments

--- src/com/shpp/p2p/cs/ohololobov/assignment9/ParsedVariableRecord.java
+++ src/com/shpp/p2p/cs/ohololobov/assignment9/ParsedVariableRecord.java
@@ -2,5 +2,12 @@ package com.shpp.p2p.cs.ohololobov.assignment9;
 
 import java.util.List;
 
+/**
+ * This record contains linear name of variable
+ * and result of parsing value of variable
+ * as list of tokens in reverse Polish notation
+ * @param variableName string value of variable name
+ * @param parsedVariableValue list of string  tokens as result of parsing in reverse Polish notation
+ */
 public record ParsedVariableRecord(String variableName, List<String> parsedVariableValue) {
 }
