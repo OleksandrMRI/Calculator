cb1ba46ec97ca6a15ffdd07774b9ff3c0138c8b0
Oleksandr Hololobov <alsan3004@gmail.com>
2026-08-28 00:13:25 +0200

    ENUM Bracket. Add comments

--- src/com/shpp/p2p/cs/ohololobov/assignment9/Bracket.java
+++ src/com/shpp/p2p/cs/ohololobov/assignment9/Bracket.java
@@ -1,18 +1,36 @@
 package com.shpp.p2p.cs.ohololobov.assignment9;
 
+/**
+ * ENUM contains opening and closing brackets although logic of handling of ENUM`s objects
+ */
 public enum Bracket{
     OPENING_BRACKET("("),
     CLOSING_BRACKET(")");
-
+    /**
+     * string value of Bracket
+     */
     private final String value;
 
+    /**
+     * constructor of instance of Bracket
+     * @param value string value of Bracket
+     */
     Bracket(String value) {
         this.value = value;
     }
 
+    /**
+     * getting string value of Bracket instance
+     * @return string value of Bracket instance
+     */
     public  String getValue() {
         return this.value;
     }
+
+    /**
+     * method create string of values of all instances of Bracket
+     * @return bracket instances values as string
+     */
     public static String bracketToString(){
         StringBuilder sb = new StringBuilder();
         for(Bracket bracket : Bracket.values()){
