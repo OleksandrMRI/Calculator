a60d1c2e81df351feadc117fcebffb5467b230f6
Oleksandr Hololobov <alsan3004@gmail.com>
2026-08-28 00:18:27 +0200

    FunctionCalculatingInterface add comments

--- src/com/shpp/p2p/cs/ohololobov/assignment9/FunctionCalculatingInterface.java
+++ src/com/shpp/p2p/cs/ohololobov/assignment9/FunctionCalculatingInterface.java
@@ -1,5 +1,8 @@
 package com.shpp.p2p.cs.ohololobov.assignment9;
 
+/**
+ * Interface for implementation in any form of calculators
+ */
 public interface FunctionCalculatingInterface {
     double calculate (double number);
 }
