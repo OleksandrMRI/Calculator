5bcbb240dca6d8cd98e086fb7b9d610cd980368a
Oleksandr Hololobov <alsan3004@gmail.com>
2026-08-28 00:38:32 +0200

    VariableEqualityRecord. Add comments

--- src/com/shpp/p2p/cs/ohololobov/assignment9/VariableEqualityRecord.java
+++ src/com/shpp/p2p/cs/ohololobov/assignment9/VariableEqualityRecord.java
@@ -1,5 +1,13 @@
 package com.shpp.p2p.cs.ohololobov.assignment9;
 
-public record VariableEqualityRecord(String variableValue, boolean unaryMinus, String sourceVariableEquality) {
+/**
+ * Record with fields that represent base information about input vatiable equality
+ * @param variableName variable name
+ * @param variableValue second element of array from intup variable equality,
+ *                      after splitting with "=" as separator
+ * @param unaryMinus boolean is true when in input variable equality war "-" before variable name
+ * @param sourceVariableEquality formated source string of variable equality without spaces in lowercase and with
+ */
+public record VariableEqualityRecord(String variableName, String variableValue, boolean unaryMinus, String sourceVariableEquality) {
 
 }
