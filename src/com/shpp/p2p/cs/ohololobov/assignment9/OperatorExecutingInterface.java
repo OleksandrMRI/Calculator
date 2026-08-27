9c5e64e13f96968fe803d51400be46b41b6991e3
Oleksandr Hololobov <alsan3004@gmail.com>
2026-08-28 00:27:56 +0200

    OperatorExecutingInterface. Add comments

---
+++ src/com/shpp/p2p/cs/ohololobov/assignment9/OperatorExecutingInterface.java
@@ -0,0 +1,8 @@
+package com.shpp.p2p.cs.ohololobov.assignment9;
+
+/**
+ * Functional interface for calculating operation with math operator
+  */
+public interface OperatorExecutingInterface {
+    double calculate(double a, double b);
+}
